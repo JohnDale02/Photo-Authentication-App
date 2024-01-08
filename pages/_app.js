f462d4d80224a5f0f3233f3f7ac49465927aa21a
John Dale <johnkdale02@gmail.com>
2024-01-08 02:53:11 -0500

    Path change

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 // pages/_app.js
 import Head from 'next/head';
-import '@/styles/globals.css';
+import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
   return (
