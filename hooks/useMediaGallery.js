6ebdb34dae41b33b0bf2cfebcbf355a3f9f1855e
John Dale <johnkdale02@gmail.com>
2024-04-06 14:42:01 -0400

    changed photo App for fingerprints

--- hooks/useMediaGallery.js
+++ hooks/useMediaGallery.js
@@ -1,16 +1,16 @@
 import { useState, useEffect, useCallback } from 'react';
 import { setMediaFromS3 } from '../cognito/config';
 
-const useMediaGallery = (cameraNumber) => {
+const useMediaGallery = (bucketName_fingerprint) => {
   const [images, setImages] = useState([]);
   const pollInterval = 20000; // Polling interval in milliseconds (e.g., 3000 for 3 seconds)
   //const pollInterval = 60000; // Polling interval in milliseconds (e.g., 3000 for 3 seconds)
 
   const fetchImages = useCallback(() => {
-    if (cameraNumber) {
-      setMediaFromS3(setImages, cameraNumber);
+    if (bucketName_fingerprint) {
+      setMediaFromS3(setImages, bucketName_fingerprint);
     }
-  }, [cameraNumber]);
+  }, [bucketName_fingerprint]);
 
   useEffect(() => {
     fetchImages(); // Initial fetch
